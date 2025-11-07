4df1d65276b1306e8b40ba318dfc7937967c477d
au633355 <au633355@uni.au.dk>
2025-11-07 12:07:45 +0100

    made basic changes to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-
+  const dinners = [{
+    id: 0,
+    host: 'UKH',
+    event: 'Fællesspisning',
+    date: '07-11-2025', //TODO turn into unix time
+  },
+  {
+    id: 1,
+    host: 'De Syv Munde',
+    event: 'Fællesspisning',
+    date: '10-11-2025',
+  },
+  {
+    id: 2,
+    host: 'FLOR',
+    event: 'Fællesspisning',
+    date: '20-11-2025',
+  },
+  {
+    id: 3,
+    host: 'UngK',
+    event: 'Fællesspisning',
+    date: '10-11-2025',
+  }];
 
 export default function Home() {
+  const listItems = dinners.map(dinner => <li key={dinner.id}> {dinner.host} </li>);
+
   return (
     <div>
-      <p> This is the homepage</p>
+      <h2>Fællesspisning</h2>
+
+      <ul>
+        {listItems}
+      </ul>
+      
     </div>
   );
 }
